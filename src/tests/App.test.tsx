5362a2eb127dd20d1495e5da4bad14154dceda88
ken4ro111 <all.depend.on.myself11@gmail.com>
2026-05-15 02:22:37 +0900

    testファイルをtests配下に移行

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { App } from "./App";
+import { App } from "../App";
 
 describe("App", () => {
   it("HOME画面を表示する", () => {
