902a41b0dec9143c29b502eabef53e95b5f9c5db
ken4ro111 <all.depend.on.myself11@gmail.com>
2026-05-15 02:22:19 +0900

    ChakuraUIを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 export const App = () => {
-  return <div>HOME画面</div>;
+  return (
+    <div>
+      <h1>HOME画面</h1>
+      <Button colorScheme="teal">Button</Button>
+    </div>
+  );
 };
